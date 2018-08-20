6b03865a39063d4034af38fdb5b97c947f9a9de2
Franz <franz.weidmann@cs.hs-fulda.de>
2018-08-20 15:54:29 +0200

    added reporter thread

--- Logging/ryu-controller/collector.py
+++ Logging/ryu-controller/collector.py
@@ -9,22 +9,39 @@ from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
 
+import threading
+import time
+
+class reporter(threading.Thread):
+   def __init__(self, threadID, reporterFunc):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = "Reporter"
+
+   def run(self):
+      print "Starting " + self.name +  " thread"
+      
+      while True:
+        self.reporterFunc(event.dp, 2)
+        self.reporterFunc(event.dp, 3)
+        self.reporterFunc(event.dp, 4)
+        sleep(5)
+
+      print "Exiting " + self.name
+
 class L2Switch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(L2Switch, self).__init__(*args, **kwargs)
-
-        self.reqPortStats(event.dp, 2)
-        self.reqPortStats(event.dp, 3)
-        self.reqPortStats(event.dp, 4)
+        reporterThread = reporter(1, reqPortStats)
+        reporterThread.start()
 
     # reply event for port stats of an VSwitch
     @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
     def portStatsReply(self, event):
         stats = event.msg.body[0]
-        print("NEW PORT STATS")
-        print(stats.port_no, stats.tx_packets)
+        self.saveStatstoJSON(stats)
 
     # enter/leave event for a VSwitch
     @set_ev_cls(dpset.EventDP, MAIN_DISPATCHER)
@@ -39,3 +56,9 @@ class L2Switch(app_manager.RyuApp):
     #  new OpenFlow message from VSwitch
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
+        console.log(ev.dp.id)
+
+    # save new stats to json file
+    def saveStatstoJSON(stats):
+        print("NEW PORT STATS")
+        print(stats.port_no, stats.tx_packets)
